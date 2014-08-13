bd7a9c8f2357c0b353549c904bf20fb2cf41bb6b
Petar Popstoyanov <p.popstoyanov@abv.bg>
2014-08-13 13:05:48 +0300

    Adding new JavaScript projects

---
+++ Introduction in JavaScript. JS instruments/Problem 4. Circle Area/circle-area.js
@@ -0,0 +1,7 @@
+﻿function calcCircleArea(r) {
+    var area = Math.PI * r * r;
+    return area;
+}
+document.getElementById("firstarea").innerHTML = calcCircleArea(7);
+document.getElementById("secondarea").innerHTML = calcCircleArea(1.5);
+document.getElementById("thirdarea").innerHTML = calcCircleArea(20);
